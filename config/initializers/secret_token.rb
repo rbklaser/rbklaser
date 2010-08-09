e45dd0478d9ce862b2a2e87085c9456b72ed9d9f
rbklaser <rbklaser@toshiba.(none)>
2010-08-09 22:18:05 +0200

    struktura rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rbklaser::Application.config.secret_token = 'd8267e12c5b764d4c4627e530445da1bdd7d25f1fe6e47636b420a5f0e09eba96f4b92b72026ff86cff265ecf73d2358bfa4c688a894788e88b240f47d831b37'
