4b0ac77f27a29fc407b3ac3e6f5ca93b8504a3dd
rbklaser <rbklaser@toshiba.(none)>
2010-08-21 13:12:33 +0200

    Usuwa takze wszystkie dzieci rodzica

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,6 +1,6 @@
 class Exam < ActiveRecord::Base
-	has_many :comments
-	has_many :resources
+	has_many :comments, :dependent => :destroy
+	has_many :resources, :dependent => :destroy
 	# Nie ma potrzeby
 	# accepts_nested_attributes_for :resources, :allow_destroy => true
 	validates_presence_of :description 
